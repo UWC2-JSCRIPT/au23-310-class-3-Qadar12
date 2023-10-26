06c90e52ed3799a07fe5329d1fc18c1b4dddc409
qadar omar <omarqadar3@gmail.com>
2023-10-25 18:50:21 -0700

    spaceShip class built and logged

--- spaceShip.js
+++ spaceShip.js
@@ -1,9 +1,28 @@
 // 1. Create a class function SpaceShip
 // - should set two properties: name and topSpeed
-// - should have a method accelerate that logs to the console 
+// - should have a method accelerate that logs to the console
 //   `${name} moving to ${topSpeed}`
 
+class SpaceShip {
+  constructor(name, topSpeed) {
+    this.name = name;
+    this.topSpeed = topSpeed;
+  }
 
+  //class method
+  accelerate() {
+    const { name, topSpeed } = this;
+    console.log(`${name} moving to ${topSpeed}`);
+  }
+}
 
-// 2. Call the constructor with a couple ships, 
+// 2. Call the constructor with a couple ships,
 // and call accelerate on both of them.
+
+const enterprise = new SpaceShip("USS Enterprise", 3300);
+const endeavour = new SpaceShip("Endeavour", 1800);
+const redDwarf = new SpaceShip("Red Dwarf", 200000);
+
+enterprise.accelerate();
+endeavour.accelerate();
+redDwarf.accelerate();
