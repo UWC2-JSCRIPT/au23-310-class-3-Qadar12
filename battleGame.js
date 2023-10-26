1c3ed263bd7677a85d79453e8ca2f75668e1fd0b
qadar omar <omarqadar3@gmail.com>
2023-10-25 17:21:01 -0700

    done with game code, func and arrow func

--- battleGame.js
+++ battleGame.js
@@ -1,16 +1,48 @@
 // 1. Create attack function below.  This will take the following parameters:
 // attackingPlayer, defendingPlayer, baseDamage, variableDamage
 
+/*const attack = function (
+  attackingPlayer,
+  defendingPlayer,
+  baseDamage = 1,
+  variableDamage = 10
+) {
+  const totalDamage =
+    baseDamage + Math.floor(Math.random() * (variableDamage + 1));
 
+  defendingPlayer.health -= totalDamage;
+
+  return `${attackingPlayer.name} hits ${defendingPlayer.name} for ${totalDamage} damage. `;
+};
+*/
 
 // 2. Create player1 and player2 objects below
 // Each should have a name property of your choosing, and health property equal to 10
 
+const player1 = {
+  health: 10,
+  name: "M.Bison",
+};
 
+const player2 = {
+  health: 10,
+  name: "Ryu",
+};
 
 // 3. Refactor attack function to an arrow function.  Comment out function above.
+const attack = (
+  attackingPlayer,
+  defendingPlayer,
+  baseDamage = 1,
+  variableDamage = 10
+) => {
+  const totalDamage =
+    baseDamage + Math.floor(Math.random() * (variableDamage + 1));
+  defendingPlayer.health -= totalDamage;
+  return `${attackingPlayer.name} hits ${defendingPlayer.name} for ${totalDamage} damage. `;
+};
 
-
+attack(player1, player2);
 
 // DO NOT MODIFY THE CODE BELOW THIS LINE
 // Set attacker and defender.  Reverse roles each iteration
