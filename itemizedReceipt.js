dfce338aedc2ee17ded5512aefe3a56fd901f639
qadar omar <omarqadar3@gmail.com>
2023-10-25 17:50:21 -0700

    done with itemizedReceipt

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -3,13 +3,20 @@
 // i.e. {descr: 'Coke', price: 1.99}
 // function should log each item to the console and log a total price
 
-
+const logReceipt = (...params) => {
+  let total = 0;
+  params.forEach((item) => console.log(`${item.descr} - ${item.price}`)); // grab items and prices
+  params.forEach((cost) => {
+    total += cost.price; // grab price and add to total
+  });
+  console.log(`Total - $${total}`); // total is ouside the forEach
+};
 
 // Check
 logReceipt(
-  { descr: 'Burrito', price: 5.99 },
-  { descr: 'Chips & Salsa', price: 2.99 },
-  { descr: 'Sprite', price: 1.99 }
+  { descr: "Burrito", price: 5.99 },
+  { descr: "Chips & Salsa", price: 2.99 },
+  { descr: "Sprite", price: 1.99 }
 );
 // should log something like:
 // Burrito - $5.99
