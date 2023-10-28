b5bd946f2ed1762480afda3cf6d4681fe18ab28b
qadar omar <omarqadar3@gmail.com>
2023-10-28 14:33:41 -0700

    done with soccer points and records

--- phoneNumber.js
+++ phoneNumber.js
@@ -1,12 +1,14 @@
-// Create a function testPhoneNumber 
+// Create a function testPhoneNumber
 // takes in a phoneNumber string in one of these formats:
 // '(206) 333-4444'
 // '206-333-4444'
 // '206 333 4444'
 // Returns true if valid, false if not valid
+const ph = '206 333 4444")';
+const regex = new RegExp("/^d{3}[-s]?d{3}[-s]?d{4}$/");
+const testPhoneNumber = (phoneNum) => regex.test(phoneNum);
 
-
-
+console.log(testPhoneNumber(ph));
 // Explanation of RegExp
 // ^      start of line
 // \(     optional start parenthesis
@@ -19,22 +21,22 @@
 // $      end of line
 
 // check testPhoneNumber
-console.log(testPhoneNumber('(206) 333-4444')); // should return true
-console.log(testPhoneNumber('(206) 33-4444')); // should return false, missing a digit
+//console.log(testPhoneNumber("(206) 333-4444")); // should return true
+//console.log(testPhoneNumber("(206) 33-4444")); // should return false, missing a digit
 
-
-// 1. Create a function parsePhoneNumber that takes in a phoneNumber string 
+// 1. Create a function parsePhoneNumber that takes in a phoneNumber string
 // in one of the above formats.  For this, you can *assume the phone number
 // passed in is correct*.  This should use a regular expression
 // and run the exec method to capture the area code and remaining part of
 // the phone number.
 // Returns an object in the format {areaCode, phoneNumber}
 
+// use a regex and then .match(regex) that will put in a array then log it out
 
-
-// Check parsePhoneNumber
-console.log(parsePhoneNumber('206-333-4444'));
+const parsePhoneNumber = (phoneNum) =>
+  // Check parsePhoneNumber
+  console.log(parsePhoneNumber("206-333-4444"));
 // returns {areaCode: '206', phoneNumber: '3334444'}
 
-console.log(parsePhoneNumber('(222) 422-5353'));
+console.log(parsePhoneNumber("(222) 422-5353"));
 // returns {areaCode: '222', phoneNumber: '4225353'}
